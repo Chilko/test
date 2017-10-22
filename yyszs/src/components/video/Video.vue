98e5339aa0290f7fb66f01abf7c4aacdf2808211
Cliock_zhuo <cliock_zhuo@163.com>
2017-10-22 13:39:25 +0800

    E:\Git\gitcangku\yyszs

---
+++ yyszs/src/components/video/Video.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    i am video
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
